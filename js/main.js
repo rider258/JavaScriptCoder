a5cf57a730c22f80d37a9292c260f5ec7760c11e
David Ulloa <david.ulloa@outlook.com>
2023-05-15 10:18:31 -0500

    En esta entrega quise modificar el codigo de la primera entrega para integrar la funcionalidad de poder buscar un estudiante

--- js/main.js
+++ js/main.js
@@ -1,14 +1,74 @@
 
-// Obtener referencia al botón con descuento
-const descuento = document.getElementById("descuento");
 
-// Agregar evento de clic al botón sin descuento
-descuento.addEventListener("click", calcularPrecio);
+
+
+/* En esta entrega quise modificar el codigo ya entregado con el fin de poner en práctica la inclusion o el llamado de las funciones en un proceso unitario. se modifico el codigo original y se anexo el código para armar una base de datos que almacenára los nombres y datos personales de los estudiantes que quieran un descuento en la matrícula de la universidad. Se creo un array de los estudiantes y asi mismo se creo un constructor de objetos para llenar el arreglo */
+
+
+
+
+// Obtener referencia al botón
+
+const nuevoEstudiante = document.getElementById("estudiante");
+
+// Agregar evento de clic al botón 
+nuevoEstudiante.addEventListener("click", regEstudiante);
+
+// Crear arreglo global de estudiantes
+const estudiantes = [];
+
+// Constructor del objeto Estudiante
+class Estudiante {
+  constructor(nombre, edad, email, telefono, carrera) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.email = email;
+    this.telefono = telefono;
+    this.carrera = carrera;
+  }
+}
+let nombre = ""// tuve que declarar esta variable como globlal para poder utilizarla en otra funcion.
+// Función para registrar un estudiante y mostrar en alert
+function regEstudiante() {
+  nombre = prompt("Ingrese su Nombre");
+  let edad = prompt("Ingrese su edad");
+  let email = prompt("Ingrese su email");
+  let telefono = prompt("Ingrese su telefono");
+  let carrera = prompt("Ingrese la carrera a la que pertenece");
+
+  let estudiante = new Estudiante(nombre, edad, email, telefono, carrera);
+  
+
+  // Guardar el nuevo estudiante en el arreglo de estudiantes
+  estudiantes.push(estudiante);
+
+  // ejecutar
+  mostrarEstudiantes(); //se invoca la funcion que indexa la info en la base de datos.
+  calcularPrecio(); //Se invoca la funcion del descuento.
+}
+
+// Función para mostrar los estudiantes en alert
+function mostrarEstudiantes() {
+  let mensaje = "Estudiantes registrados:\n\n"; //las n significan nueva linea
+  estudiantes.forEach(function (estudiante) {
+    mensaje += `Nombre: ${estudiante.nombre}\nEdad: ${estudiante.edad}\nEmail: ${estudiante.email}\nTeléfono: ${estudiante.telefono}\nCarrera: ${estudiante.carrera}\n`; // las n significan nueva linea
+  });
+
+  // Mostrar mensaje en alert
+  alert(mensaje);
+}
+
+
 
 //crear la función que almacena el nombre y el codigo de descuento 
 function calcularPrecio() {
-    const nombre = prompt("Como te llamas?");
-    const carrera = prompt("Ingresa la modalidad que quieres cotizar, Ingenieria, Liceciatura o Postgrado");
+
+    
+    const carrera = prompt(`Hola ${nombre} Ingresa la modalidad que quieres cotizar:\n 
+   1: Ingenieria\n
+   2: Liceciatura\n
+   3: Postgrado`);
+   
     let precio = 0;
     let nuevoPrecio = 0;
     let codigoDcto
@@ -89,3 +149,41 @@ function calcularPrecio() {
             return;
     }
 }
+
+const buscarEstudiante = document.getElementById("buscar");
+
+// Agregar evento de clic al botón 
+buscarEstudiante.addEventListener("click", findStudent);
+
+function findStudent(){
+  const nombreBuscado = prompt("Ingrese el nombre del estudiante a buscar");
+
+const estudiantesEncontrados = estudiantes.filter(estudiante => estudiante.nombre.toLowerCase() === nombreBuscado.toLowerCase());
+
+if (estudiantesEncontrados.length > 0) {
+  console.log("Estudiantes encontrados:");
+  estudiantesEncontrados.forEach(estudiante => {
+    alert(`Nombre: ${estudiante.nombre}\n
+    Edad: ${estudiante.edad}
+    Email: ${estudiante.email}
+    Teléfono: ${estudiante.telefono}
+    Carrera: ${estudiante.carrera}
+    `);
+    
+  });
+} else {
+  alert("No se encontraron estudiantes con ese nombre.");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
